4187c8f95105416ada435cb163304afc83402bb4
Le Hoan <nightmare3198@gmail.com>
2026-05-05 19:01:23 +0700

    add pahoMQTT

--- main/main.c
+++ main/main.c
@@ -9,26 +9,14 @@
 #include "nvs_init_in_main.h"
 // from: "components/esp32_peripherals" folder
 #include "wifi_sta_ap.h"
-#include "URI_CONFIG.h"
-// from: "components/coreHTTP" folder
-#include "core_http_execute.h"
+// from: "components/pahoMQTT" folder
+#include "MQTTAsync.h"
 
-// static char* TAG = "Main_Example";
+void app_main(void) {
 
-void app_main(void)
-{
-    nvs_flash_init_in_main();
-
-    wifi_init_sta();
-    WIFI_WAIT_CONNECT(wifi_event_group);
-                                                                
-    dns_lookup();
-    DNS_WAIT_CONNECT(event_group);
-                                                                
-    requestHeaderInit();
-    xTaskCreate(sendRequest, "tcp_server", 8192, NULL, 5, NULL);
-    printf(">>>>Main end<<<<\n");
 }
+
+
 
 
 
