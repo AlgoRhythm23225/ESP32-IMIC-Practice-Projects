9d8f71d3090b2e49939e1bfd5c75a98f308ab9cd
Le Hoan <nightmare3198@gmail.com>
2026-05-13 17:06:52 +0700

    Update AWS IOT CORE

--- main/main.c
+++ main/main.c
@@ -13,19 +13,34 @@
 #include "esp_http_client.h"
 #include "cJSON.h"
 #include "esp_crt_bundle.h"
-#include "tcp_udp_socket.h"
+#include "wifi_sta_ap.h"
 esp_mqtt_client_handle_t client = NULL;
 const char *TAG = "[MQTT]";
+
+#define THINGNAME "Esp32"
+
+// Amazone ROOT CA
+extern const uint8_t root_ca_pem_start[] asm("_binary_AmazonRootCA1_pem_start");
+extern const uint8_t root_ca_pem_end[] asm("_binary_AmazonRootCA1_pem_end");
+
+extern const uint8_t device_pem_crt_start[] asm("_binary_Device_Certificate_crt_start");
+extern const uint8_t device_pem_crt_end[] asm("_binary_Device_Certificate_crt_end");
+
+extern const uint8_t private_pem_key_start[] asm("_binary_Private_Key_key_start");
+extern const uint8_t private_pem_key_end[] asm("_binary_Private_Key_key_end");
+#define MQTT_TOPIC_WEATHER  "esp32_home/weather"  
 
 void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
     esp_mqtt_event_handle_t event = event_data;
 
     switch ((esp_mqtt_event_id_t)event_id) {
+        // When connect successfully, subcribe to a topic to inform the online status
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT connected");
 
             // subcribe topic
-            esp_mqtt_client_subscribe(client, "esp32/led", 0);
+            esp_mqtt_client_subscribe(client, "esp32/led", 1);
+            esp_mqtt_client_subscribe(client, MQTT_TOPIC_WEATHER, 1);
 
             // try publish
             esp_mqtt_client_publish(client, "esp32/led", "online", 0, 0, 0);
@@ -61,7 +76,10 @@ void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event
 
 void mqtt_app_start(void) {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = "mqtt://broker.emqx.io:1883"
+        .broker.address.uri = "mqtts://a1974gzwyohsln-ats.iot.us-east-1.amazonaws.com:8883",
+        .broker.verification.certificate = (const char *)root_ca_pem_start,
+        .credentials.authentication.certificate = (const char *)device_pem_crt_start,
+        .credentials.authentication.key = (const char *)private_pem_key_start,
     };
 
     client = esp_mqtt_client_init(&mqtt_cfg);
@@ -72,7 +90,6 @@ void mqtt_app_start(void) {
 }
 
 #define WEATHER_API_URL     "http://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=bf7911260e33a6b7ebd42f893fbe368a&units=metric"
-#define MQTT_TOPIC_WEATHER  "esp32_home/weather"  
 char response_data[1024];
 int response_len = 0;
 esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
@@ -115,8 +132,8 @@ void parse_weather_data(const char *json_string) {
             snprintf(message, sizeof(message), "Temperature: %.2fºC\nHumidity: %d%%", temp->valuedouble, humidity->valueint);
             esp_mqtt_client_publish(client, "esp32_home/weather", message, 0, 1, 0);
         }
-
     }
+    cJSON_Delete(root);
 }
 
 void weather_task(void *pvParameters) {
@@ -135,15 +152,17 @@ void weather_task(void *pvParameters) {
             if (err == ESP_OK) {
                 ESP_LOGI("[HTTP]", "HTTP GET Status = %d", esp_http_client_get_status_code(http_client));
                 parse_weather_data(response_data);
-                ESP_LOGI("[MQTT]", "Sent MQTT");
+                ESP_LOGI("[MQTT]", "Sent MQTT, free heap: %d", esp_get_free_heap_size());
+                if (esp_get_free_heap_size() < 100000) {
+                    esp_restart();
+                }
             } else {
                 ESP_LOGE("[HTTP]", "HTTP GET request failed: %s", esp_err_to_name(err));
-                ESP_LOGE("[HTTP]", "HTTP GET Status = %d", esp_http_client_get_status_code(http_client));
+                ESP_LOGE("[HTTP]", "HTTP GET Status = %d", esp_http_client_get_status_code(http_client));           
             }   
-
             esp_http_client_cleanup(http_client);
         }
-        vTaskDelay(pdMS_TO_TICKS(120000));
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
 
