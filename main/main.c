efe907df30abb080ee9e481ec5cc62ab856ff7d2
Le Hoan <nightmare3198@gmail.com>
2026-05-20 18:19:25 +0700

    Communication between 2 tasks using Queue

--- main/main.c
+++ main/main.c
@@ -5,48 +5,79 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_event.h"
+#include "esp_log.h"
 // from: "annoying_default_funcs" folder
 #include "nvs_init_in_main.h"
 // project
 
-bool task2_flag = false;
-char buf[100] = {0};
-int ind = 0;
+QueueHandle_t xMyQueue;
+
 void task_1(void *vParameter){
     int c;
+    int ind = 0;
+    char *pBuffer = malloc(100);
+    if (pBuffer == NULL) {
+        vTaskDelete(NULL);
+    }
+    memset(pBuffer, 0, sizeof(pBuffer));
+
     while (1) {
         c = getchar();
         if (c == EOF) {
             vTaskDelay(pdMS_TO_TICKS(10));
             continue;
         }
-        if (c != '\n') {
-            buf[ind++] = c;
-            printf("%c", c);
-        } 
-        else {
-            task2_flag = true;
+        if (c != '\n'  && c != '\r') {
+            if (ind < 99) {
+                pBuffer[ind++] = c;
+                printf("%c", c);
+            }
         }
+        else if (ind > 0) {
+            pBuffer[ind] = '\0';
+            printf("\n");
+            if (xQueueSend(xMyQueue, &pBuffer, portMAX_DELAY) == pdPASS) {
+                ESP_LOGI("[TASK 1]", "Sent string to Queue!");
+            }
+            else {
+                free(pBuffer);
+            }
+
+            pBuffer = malloc(100);
+            if (pBuffer == NULL) {
+                ESP_LOGE("[Task 1]", "Not enough RAM for allocation!");
+                vTaskDelete(NULL);
+            }
+            memset(pBuffer, 0, 100);
+            ind = 0;
+        } 
     }
 }
+
 void task_2(void *vParameter){
+    char *pReceivedBuffer = NULL;
+
     while (1) {
-        if (task2_flag) {
-            printf("\nBuf = %s\n", buf);
-            // reset state
-            memset(buf, 0, sizeof(buf));
-            task2_flag = false;
-            ind = 0;
-        }
-        vTaskDelay(pdMS_TO_TICKS(20));
+        if (xQueueReceive(xMyQueue, &pReceivedBuffer, portMAX_DELAY) == pdPASS) {
+            ESP_LOGI("[TASK 2]", "Received string: %s!", pReceivedBuffer);
+            free(pReceivedBuffer);
+            pReceivedBuffer = NULL;
+        } 
     }
 }
 
 void app_main(void) {
     nvs_flash_init_in_main();
 
-    xTaskCreate(task_1, "Task 1", 4096, NULL, 4, NULL);
-    xTaskCreate(task_2, "Task 2", 2048, NULL, 4, NULL);   
+    xMyQueue = xQueueCreate(5, sizeof(int));
+
+    if (xMyQueue != NULL) {
+        xTaskCreate(task_1, "Task 1", 4096, NULL, 4, NULL);
+        xTaskCreate(task_2, "Task 2", 2048, NULL, 4, NULL);   
+    }
+    else {
+        ESP_LOGE("[Main]", "Failed to create Queue");
+    }
 }
 
 
